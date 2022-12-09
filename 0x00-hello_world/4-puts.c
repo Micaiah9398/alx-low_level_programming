13a3fba383754343a6edf27ddd140fa3a13e516e
Micaiah <micaiahadamson1993@gmail.com>
2022-12-09 08:22:02 -0800

    task 4 updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - print a line of code using puts
+ * main - Entry point 
  *
  * Return: 0
 */
@@ -8,5 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
