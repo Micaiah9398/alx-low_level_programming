e108f75ad773c67c5c3c73e0cd9e8d865bb6b9cd
Micaiah <micaiahadamson1993@gmail.com>
2022-12-08 17:41:49 -0800

    task 4 completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-
 /**
  * main entry
  *
  *Return: Always 0 (seccess)
  */
-
 int main(void)
 {
  	puts("\"programming is like building a multllingual puzzles");
  	return (0);
-
 }
