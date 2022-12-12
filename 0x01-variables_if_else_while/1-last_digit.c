f882283f1ee7f50533318a777cb9891ed81b9e81
Micaiah <micaiahadamson1993@gmail.com>
2022-12-12 05:34:35 -0800

    task 1 complited

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,27 +1,34 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.hd>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
+{
+	int n;
+	int x;
 
-        int n;
-	int m;	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 
-	m = n % 10;
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	if (m < 6 && m | = 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-		return (0);
+	x = n % 10;
+	printf("Last digit of %d is %d", n, x);
+	if (x > 5)
+	{
+		printf("and is greater than 5");
+	}
+	if (x == 0)
+	{
+		printf("and is 0");
+	}
+	if (x < 6 && x != 0)
+	{
+		printf(" and is less than 6 and not 0");
+	}
+
+	printf("\n");
+	return (0);
 }
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,10 @@ int main(void)
 	char c;
 	c = 'a';
 	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
+	(c <= 'z') {
+	putchar(c);
+	c++;
+	}
 	putchar('\n');
 	return (0);
 }
