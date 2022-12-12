6c54a3b1e24e151f049a0c20aba2a5bfb97e282c
Micaiah <micaiahadamson1993@gmail.com>
2022-12-12 08:48:55 -0800

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,14 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char 1;
-
-	char 2;
-
-	1 = 'a';
-	2 = 'A';
-	while
-		(1 <= 'z') {
-			putchar(c);
-			1++;
-		}
-	while
-	(2 <= 'Z') {
-			putchar(c);
-			2++;
-		}
-	putchar('\n');
+	for (int x = 'A'; x <= 'Z'; x++)
+		x = tolower(x);
+	putchar(x);
 	return (0);
 }
