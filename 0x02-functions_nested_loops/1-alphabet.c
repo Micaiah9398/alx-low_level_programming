0fc85af9a45ffd9dd33b5e80918a1b0f43963498
Micaiah <micaiahadamson1993@gmail.com>
2022-12-13 20:15:03 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign -print the sign of number
+ * @n: the number of the sign to be printed
+ * Return:1 if number is greater than zero
+ * 0 of number is zero
+ * -1 if number is lessthan zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
