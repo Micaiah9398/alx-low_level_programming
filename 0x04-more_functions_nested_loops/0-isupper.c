afdfd2e30b37fa78c46c7c1e63dfefb0d8d18f1b
Micaiah <micaiahadamson1993@gmail.com>
2022-12-15 19:54:43 -0800

    task 0 to uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isuper - function that check for uppercase character.
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+{
+	for (int c = 'A'; c <= 'Z'; c++)
+		c = uppercase(c);
+	putchar(c);
+
+	return (1);
+
+	else
+		return (0);
+}
