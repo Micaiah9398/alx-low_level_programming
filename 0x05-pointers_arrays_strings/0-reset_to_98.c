c92e322eba0129831833a84f9eae0460e948de27
Micaiah <micaiahadamson1993@gmail.com>
2022-12-19 16:46:31 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 -> a function to update the reference of pointer
+ * reset_to_98 - a function to update the reference of pointer
  *@n: a pointer reset to be updated
  */
-void reset_to_98(int *n);
-
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * puts - this is a fuction that puts strings
+ * @str: a parameter to _puts functions
+ */
+void _puts(char *str)
