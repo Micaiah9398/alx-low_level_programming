93ba6dd54b327f2dd63363b805929909c25287da
Micaiah <micaiahadamson1993@gmail.com>
2022-12-19 19:03:40 -0800

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
 /**
- * puts - this is a fuction that puts strings
+ * _puts - this is a fuction that puts strings
  * @str: a parameter to _puts functions
  */
 void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+{
+	_putchar(*str);
+}
+	_putchar('\n');
+}
