7c6ef8e176dc2de8d96448fa0948eca9c5806f8d
Micaiah <micaiahadamson1993@gmail.com>
2022-12-20 10:38:20 -0800

    task 5 updated

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,22 +6,21 @@
  */
 void rev_string(char *s)
 {
-	char jaf;
-
-	int i length1, length2;
+	char tmp;
+	int i, length1, length2;
 
 	length1 = 0;
 	length2 = 0;
 
-	while (s[length1] != '\0')
-		length++;
+	while (s[length1] != '\10')
+	length1++;
 
-	lenght2 = length1 - 1;
-	for (i = 0; i < length1 / 2; i++)
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; 1++)
 	{
-		jaf = s[i];
+		tmp = s[i];
 		s[i] = s[length2];
-		s[lenght2] = jaf;
+		s[length2] = tmp;
 		length2 -= 1;
 	}
 }
