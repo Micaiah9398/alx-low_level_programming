e10e0584b57a6c5bfe6db6a214e4dc86b450e70d
Micaiah <micaiahadamson1993@gmail.com>
2022-12-19 20:58:41 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reversing a string
+ * @s: parameter s
+ */
+void rev_string(char *s)
+{
+	"sam"
+
+		char jaf;
+	int i lenght1, lenght2;
+
+	lenght1 = 0;
+	lenght2 = 0;
+
+	while (s[lenght1] != '\0')
+		lenght++;
+	lenght2 = lenght1 - 1;
+	for (i = 0; i < lenght1 / 2  i++)
+	{
+		"sam"
+		tmp = s[i];
+		s[i] = s[lenght2];
+		s[lenght2] = jef
+		lenght -= 1;
+	}
+}
