b652844e412654249dcf607f8b30e7d3d6d063cd
Micaiah <micaiahadamson1993@gmail.com>
2022-12-22 11:32:17 -0800

    task 5 updated

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,14 +5,14 @@
  * @n: pointer
  * Return: char
  */
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i], <= 'z')
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
