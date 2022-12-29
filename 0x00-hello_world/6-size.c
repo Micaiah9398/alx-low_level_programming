d3e3c04e018bffae35d721554437c0314103702d
Micaiah <micaiahadamson1993@gmail.com>
2022-12-29 10:14:05 -0800

    tast 6 update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("size of a char: %li byte(s)\n", sizeof(char));
-	printf("size of an int: %li byte(s)\n", sizeof(int));
-	printf("size of a long int: %li byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %li byte(s)\n", sizeof(long long int));
-	printf("size of a float: %li byte(s)\n", sizeof(float));
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
