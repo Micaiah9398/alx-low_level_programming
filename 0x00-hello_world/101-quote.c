6ea259ef4368347ecb011cee683c37177ad35cf3
Micaiah <micaiahadamson1993@gmail.com>
2022-12-29 10:48:58 -0800

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -Entry point
+ * Return: Always 1
+ */
+int main(viod)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
