22333b8245741bb1045fd662e6bb612246392001
Micaiah <micaiahadamson1993@gmail.com>
2022-12-31 08:37:48 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,29 +8,29 @@
  */
 int main(void)
 {
-int q;
-int w;
+	int q;
+	int w;
 
-for (q = 0; q < 100; q++)
-{
-for (w = 0; w < 100; w++)
-{
-if (w > q)
-{
-putchar(q / 10 + '0');
-putchar(q % 10 + '0');
-putchar(' ');
-putchar(w / 10 + '0');
-putchar(q % 10 + '0');
+	for (q = 0; q < 100; q++)
+	{
+		for (w = 0; w < 100; w++)
+		{
+			if (w > q)
+			{
+				putchar(q / 10 + '0');
+				putchar(q % 10 + '0');
+				putchar(' ');
+				putchar(w / 10 + '0');
+				putchar(q % 10 + '0');
 
-if (q * 100 + w != 9899)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
+				if (q * 100 + w != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 putchar('\n');
 return (0);
 }
