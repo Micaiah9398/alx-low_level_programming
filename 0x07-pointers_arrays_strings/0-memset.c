454384cda5532c3dfba02c88c2ec6a67c80e9214
Micaiah <micaiahadamson1993@gmail.com>
2023-01-03 00:41:11 -0800

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,14 +7,14 @@
  * @n: size of byte
  * Return: a pointer to the memory area @s.
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
 	while (i < n)
 	{
-		*(s + i) = b;
-		i += 1;
+		[i] = b;
+		i++;
 	}
 	return (s);
 
