c0b30933c572500d3e97fc8b6d64095c2139e7bd
Micaiah <micaiahadamson1993@gmail.com>
2023-01-03 18:47:34 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j, bool;
+	unsigned int i, j, bool;
 
-for (i = 0; *(s + i) != '\0'; i++)
-{
-bool = 1;
-
-for (j = 0; *(accept + j) != '\0'; j++)
-{
-for (*(s + i) == *(accept + j))
-{
-bool = 0;
-break;
-}
-}
-for (bool == 1)
-break;
-}
-return (i);
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+		if (bool == 1)
+			break;
+	}
+	return (i);
 }
