8bd1069483681646fecf009b85fe0710efe675ac
Micaiah <micaiahadamson1993@gmail.com>
2023-01-03 16:48:36 -0800

    task 0 - 9

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int _putchar(char c)
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
