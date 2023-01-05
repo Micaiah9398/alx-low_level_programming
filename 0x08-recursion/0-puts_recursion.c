20ed95790b84d1135fe5a155f348f1a29b7aceea
Micaiah <micaiahadamson1993@gmail.com>
2023-01-05 15:01:10 -0800

    task 0 updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
-ncllude "main.h"
+#include "main.h"
 
 /**
  * _puts_recursion - prints a string, followed by a new line.
  * @s: string
- * Return: Always 0
+ * Return: no return.
  */
 void _puts_recursion(char *s)
 {
