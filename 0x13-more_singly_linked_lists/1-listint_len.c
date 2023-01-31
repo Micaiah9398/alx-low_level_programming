141d608bd8f45e6625ce0903cc47dddb650cf477
micaiah <micaiahadamson1993@gmail.com>
2023-01-30 17:32:38 -0800

    length of a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,12 +7,12 @@
  */
 size_t listint_len(const listint_t *h)
 {
-size_t i = 0;
+	size_t nnodes = 0;
 
-while (h ! = NULL)
-{
-h = h->next;
-i++;
-}
-return (i);
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
