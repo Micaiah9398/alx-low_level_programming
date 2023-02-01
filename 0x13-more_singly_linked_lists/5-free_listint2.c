ddc56eda7bb97aaf4d86f98501f32e7cd8641054
micaiah <micaiahadamson1993@gmail.com>
2023-02-01 13:57:01 -0800

    task 5 updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,25 @@
 #include "lists.h"
+
 /**
- * free_listint- free a link
+ * free_listint2- free a link
  * @head: head of list
  *
  * Return: no return
  */
-void free_listint(listint_t **head)
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
+	listint_t *curr;
 
-	if (head == NULL)
-		return;
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		tmp = *head;
-		*head = (*head)->next;
-		free(tmp);
+		curr = *head;
+		while ((tmp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(tmp);
+		}
+		*head = NULL;
 
 	}
 }
