ce1d7f548ed635064c2c75138c8fad2206c23eaa
micaiah <micaiahadamson1993@gmail.com>
2023-02-01 13:34:22 -0800

    task 5 updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,14 +7,15 @@
  */
 void free_listint(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *tmp;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		temp = *head;
+		tmp = *head;
 		*head = (*head)->next;
-		free(temp);
+		free(tmp);
+
 	}
 }
