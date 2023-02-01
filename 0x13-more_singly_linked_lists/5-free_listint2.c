b962cd105ef1fee9cb55f07ede4342543c39582f
micaiah <micaiahadamson1993@gmail.com>
2023-02-01 02:26:56 -0800

    free list string to a string

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint- free a link
+ * @head: head of list
+ *
+ * Return: no return
+ */
+void free_listint(listint_t **head)
+{
+	listint_t **temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
