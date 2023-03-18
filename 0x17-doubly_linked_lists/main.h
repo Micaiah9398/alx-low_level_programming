7f63fa57c399741b8233c13c55aec656551ad185
micaiah <micaiahadamson1993@gmail.com>
2023-03-17 21:13:02 -0700

    the list of both backward direction and forward direction

--- 0x17-doubly_linked_lists/main.h
+++ 0x17-doubly_linked_lists/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
